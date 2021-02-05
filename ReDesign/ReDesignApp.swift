611dc5b387dedf6dd8fc8b084aefed108777b026
Andrej Kling <boxingprofi@web.de>
2021-02-05 19:50:32 +0100

    Initial Commit

---
+++ ReDesign/ReDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReDesignApp.swift
+//  ReDesign
+//
+//  Created by Andrej Kling on 05.02.21.
+//
+
+import SwiftUI
+
+@main
+struct ReDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
